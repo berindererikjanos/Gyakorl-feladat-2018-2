6c5b68073ac42927a0e6ccbf0b59d120775bc665
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-04-25 11:52:06 +0200

    Update palindroma.py

--- palindroma.py
+++ palindroma.py
@@ -1,1 +1,14 @@
 
+def palindroma_e(s): #palindróma: visszafelé is ugyanaz
+    s2 = s[::-1]
+    if s == s2:
+        return True
+    else:
+        return False
+
+
+s = input("Adjon meg egy szot: ")
+if palindroma_e(s):
+    print("Palindroma")
+else:
+    print("Nem palindroma")
