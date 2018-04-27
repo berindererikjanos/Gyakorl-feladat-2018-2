64b6816a522d65dc80d17ff87a8926bd5d1ea86d
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-04-27 11:45:49 +0200

    Update szamjegyek_db.py

--- szamjegyek_db.py
+++ szamjegyek_db.py
@@ -1,1 +1,12 @@
-h
+#Írjon függvényt mely a bementi szóra megszámolja, hogy hány darab számjegy van benne
+
+def szamjegyek_db(s):
+    db = 0
+    for i in range(len(s)): #végigmegyünk a szón
+        if s[i].isdigit(): #ha az adott karaktere számjegy
+            db+=1 #akkor a darab változót 1-gyel növeljük
+    return db
+
+
+s=input()
+print(szamjegyek_db(s))
