214e1048557d19ba48527f65aa01ba588cb51660
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-04-27 11:44:40 +0200

    Update van_paros_elem.py

--- van_paros_elem.py
+++ van_paros_elem.py
@@ -1,1 +1,12 @@
+# Kérjen be egy 5 elemű listát és vizsgálja meg, hogy szerepel e benne páros szám
 
+
+def minden_paros(lista):
+    for i in range(len(lista)):
+        if lista[i] % 2 == 0: #elég 1x igaznak lennie az ifnek. Elég 1db páros számot találni
+            return True
+    return False #ha 1x sem volt igaz a fenti if, akkor false, hiszen akkor nem volt páros szám
+
+
+lista = [1, 3, 5, 7, 5]
+print(minden_paros(lista))
