cf94f793c75a0fcac0f9f81ba0386d721e26a087
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-05-03 23:14:44 +0200

    Update lambda_logikaiertek.py

--- lambda_logikaiertek.py
+++ lambda_logikaiertek.py
@@ -1,3 +1,13 @@
 import math
 
-#def area(r) 
+def area(r):
+  a = r**2*math.pi
+  return a
+
+ls = [4,5,6,7,8,9,234,21]
+for i in ls:
+  print("Kör területe: ",area(i))
+ 
+area =lambda r : r**2*math.pi
+ls = [6,21,14,563,123,56]
+print("Adott számok területe: ",list(map(lambda r : r**2*math.pi,[5,10,24,52,56,21,45])))
